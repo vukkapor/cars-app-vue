0c851559bba93d4d2104037a7d6702b5c368aa78
Vuk Kapor <vukkapor12@gmail.com>
2019-07-02 18:47:20 +0200

    added router and cars

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,25 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+      <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav">
+          <li class="nav-item">
+            <router-link to="/cars">Cars</router-link>
+          </li>
+        </ul>
+      </div>
+    </nav>
+    <router-view />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from "./components/HelloWorld.vue";
 
 export default {
-  name: 'app',
+  name: "app",
   components: {
     HelloWorld
   }
-}
+};
 </script>
-
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,26 @@
 import Vue from 'vue'
 import App from './App.vue'
+import VueRouter from 'vue-router'
+
+Vue.use(VueRouter)
+
+import AppCars from '@/components/AppCars'
+
+const routes = [{
+  path: '/cars',
+  component: AppCars
+}]
+
+
+const router = new VueRouter({
+  routes: routes,
+  mode: 'history'
+})
+
 
 Vue.config.productionTip = false
 
 new Vue({
+  router: router,
   render: h => h(App),
 }).$mount('#app')
